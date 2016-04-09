b8f429e663b477eea376456ed9a93192e8885892
Kalle01 <Hoefkens@Hof-Luettgesheide.de>
2016-04-10 00:53:27 +0200

    weitere konkretisierungen

--- Quellcode/ZentraleLogik.py
+++ Quellcode/ZentraleLogik.py
@@ -1,3 +1,5 @@
+#Ausgangszustand: Station ist leer, Nachlaufsperre befindet sich in Stellung offen
+
 #Variablen
 
 VerteilZeit=20
@@ -15,13 +17,17 @@ Futter4Gewicht=5
 
 MaximaleMengeKF=500
 
-#Identifiziere Pferd
+#Identifiziere Pferd und Anlage
+If AnweSensor1 = 0:
+Schau nach dem RFIDLeser
+
 #Einlesen der Transpondernummer im Futtergang 
 Input (RFIDNr)
+Input (AnlagenNummer)
 RFIDNr=1234567890123456
+AnlagenNummer=Z0
 
 #Schaue in der DB Tabelle Pferdenamen nach welches Pferd der RFIDNr entspricht und fülle die Pferde Variablen
-
 PferdeID =10
 PferdeName="Donna"
 Fresspause=20
@@ -33,65 +39,70 @@ MengeFutter4=10
 Heu-J-N=True
 HeuZeit=240
 
-#Schaue in der DB Tabelle Fuetterungsprotokoll nach wieviel Futter das Pferd heute schon bekommen hat und fülle die Pferde Variablen
+#Schaue via SQL Statement in der DB Tabelle Fuetterungsprotokoll nach, wieviel Futter das Pferd heute und in den letzten 60 Minuten schon bekommen hat und fülle die Pferde Variablen
 
 LetztesFutterDatum=01.01.2016
 LetzteFutterZeit=00:00:00
-ZuteilungFutter1=0
-ZuteilungFutter2=0
-ZuteilungFutter3=0
-ZuteilungFutter4=0
-HeuraumZutritt=False
-ZuteilungHeuZeit=0
+MengeStd=0
+ZuteilungFutter1aufgelaufen=0
+ZuteilungFutter2aufgelaufen=0
+ZuteilungFutter3aufgelaufen=0
+ZuteilungFutter4aufgelaufen=0
+ZuteilungHeuZeitaufgelaufen=0
 
- 
 #Zentrale Fütterungslogik
 
 #Zuerst das Heu
 
 if Heu-J-N=True:
 	print ("Pferd ", PferdeName, " bekommt Heu")
-	FunkHeuTuer(1)# hier folgt die Function FunkHeuTuer 1 öffnen
+	FunkHeuTuer(AnlagenNummer)# hier folgt die Function FunkHeuTuer öffnen mit Parameter Anlage in dem das Pferd steht
 	
-elif HeuZeit/VerteilZeit*(ZyklusStart-AktZeit) >ZuteilungHeuZeit:
+elif HeuZeit/VerteilZeit*(ZyklusStart-AktZeit) >ZuteilungHeuZeit and AnlagenNummer = H1-9:
 	print ("Pferd ", PferdeName, " bekommt ", Futter2Name)
-	FunkHeuSchieber(1)# hier folgt die Function FunkHeuSchieber 1 füttern	
-
+	FunkHeuSchieber(AnlagenNummer)# hier folgt die Function FunkHeuSchieber 1 füttern mit Parameter Anlage in dem das Pferd steht
 else:
 	print ("Pferd ", PferdeName, " bekommt zur Zeit kein Heu ")	
 	
 	
 #Jetzt das Kraftfutter	
 
-if AktZeit - LetzteFutterZeit >= Fresspause and MengeStd <= MaximaleMengeKF:
-	if MengeFutter1/VerteilZeit*(ZyklusStart-AktZeit) + Futter1Gewicht >=ZuteilungFutter1:
-		print ("Pferd ", PferdeName, " bekommt ", Futter1Name)
-		FunkMotor(Motor1) # hier folgt die Function FunkMotor füttern mit Ansteuerung Motor1
-		save to DB AktZeit PferdeID Futter1Gewicht 
+if AnlagenNummer = (Z0, K0-K9) and MengeStd <= MaximaleMengeKF:
+	if MengeFutter1/VerteilZeit*(ZyklusStart-AktZeit) + Futter1Gewicht >=ZuteilungFutter1aufgelaufen and AktZeit - LetzteFutterZeit >= Fresspause:
+		# Schließen der Nachlaufsperre
+		FunkNachlauf(AnlagenNummer) # hier folgt die Function FunkNachlauf mit Parameter Anlage in dem das Pferd steht
+		print ("Pferd ", PferdeName, " bekommt ", Futter1Name, "Fütterung läuft")
+		FunkMotor(Motor1, AnlagenNummer) # hier folgt die Function FunkMotor füttern mit Parameter Anlage in dem das Pferd steht und Ansteuerung Motor1
 		# Datensatz Datum, Uhrzeit, Menge Schneckenumdrehung Futter1, Futterart in DB Tabelle Fütterungsprotokoll speichern
+		save to DB AktZeit PferdeID Futter1Gewicht Futter1Name
 		
-	elif MengeFutter2/VerteilZeit*(ZyklusStart-AktZeit) + Futter2Gewicht >=ZuteilungFutter2:
-		print ("Pferd ", PferdeName, " bekommt ", Futter2Name)
-		FunkMotor(Motor2)# hier folgt die Function FunkMotor füttern mit Ansteuerung Motor2
-		save to DB AktZeit PferdeID Futter2Gewicht
+	elif MengeFutter2/VerteilZeit*(ZyklusStart-AktZeit) + Futter2Gewicht >=ZuteilungFutter2aufgelaufen and AktZeit - LetzteFutterZeit >= Fresspause:
+		# Schließen der Nachlaufsperre
+		FunkNachlauf(AnlagenNummer) # hier folgt die Function FunkNachlauf mit Parameter Anlage in dem das Pferd steht
+		print ("Pferd ", PferdeName, " bekommt ", Futter2Name, "Fütterung läuft")
+		FunkMotor(Motor2, AnlagenNummer)# hier folgt die Function FunkMotor füttern mit Parameter Anlage in dem das Pferd steht und Ansteuerung Motor2
 		# Datensatz Datum, Uhrzeit, Menge Schneckenumdrehung Futter2, Futterart in DB Tabelle Fütterungsprotokoll speichern
-
-	
-	elif MengeFutter3/VerteilZeit*(ZyklusStart-AktZeit) + Futter3Gewicht >=ZuteilungFutter3:
-		print ("Pferd ", PferdeName, " bekommt ", Futter3Name)
-		FunkMotor(Motor3)# hier folgt die Function FunkMotor füttern mit Ansteuerung Motor3
-		save to DB AktZeit PferdeID Futter3Gewicht
+		save to DB AktZeit PferdeID Futter2Gewicht Futter2Name
+		
+	elif MengeFutter3/VerteilZeit*(ZyklusStart-AktZeit) + Futter3Gewicht >=ZuteilungFutter3aufgelaufen and AktZeit - LetzteFutterZeit >= Fresspause:
+		# Schließen der Nachlaufsperre
+		FunkNachlauf(AnlagenNummer) # hier folgt die Function FunkNachlauf mit Parameter Anlage in dem das Pferd steht
+		print ("Pferd ", PferdeName, " bekommt ", Futter3Name, "Fütterung läuft")
+		FunkMotor(Motor3, AnlagenNummer)# hier folgt die Function FunkMotor füttern mit Parameter Anlage in dem das Pferd steht und Ansteuerung Motor3
 		# Datensatz Datum, Uhrzeit, Menge Schneckenumdrehung Futter3, Futterart in DB Tabelle Fütterungsprotokoll speichern
-
+		save to DB AktZeit PferdeID Futter3Gewicht Futter3Name
 	
-	elif MengeFutter4/VerteilZeit*(ZyklusStart-AktZeit) + Futter4Gewicht >=ZuteilungFutter4:
-		print ("Pferd ", PferdeName, " bekommt ", Futter4Name)
-		FunkMotor(Motor4)# hier folgt die Function FunkMotor füttern mit Ansteuerung Motor 4
-		save to DB AktZeit PferdeID Futter1Gewicht
+	# Futter 4 ist besonders. Dieses wird immer „zeitgleich“ zu einem anderen Futter gefüttert. Hier ist, wenn vorhanden, der Mineralfutterbehälter anzuschließen
+	elif MengeFutter4/VerteilZeit*(ZyklusStart-AktZeit) + Futter4Gewicht >=ZuteilungFutter4aufgelaufen:
+		# Schließen der Nachlaufsperre
+		FunkNachlauf(AnlagenNummer) # hier folgt die Function FunkNachlauf mit Parameter Anlage in dem das Pferd steht
+		print ("Pferd ", PferdeName, " bekommt ", Futter4Name, "Fütterung läuft")
+		FunkMotor(Motor4, AnlagenNummer)# hier folgt die Function FunkMotor füttern füttern mit Parameter Anlage in dem das Pferd steht und Ansteuerung Motor4
 		# Datensatz Datum, Uhrzeit, Menge Schneckenumdrehung Futter4, Futterart in DB Tabelle Fütterungsprotokoll speichern
-
-
+		save to DB AktZeit PferdeID Futter4Gewicht Futter4Name
+		
 	else:
 		print ("Pferd ", PferdeName, " hat keinen Anspruch auf Kraftfutter")	
 else:
 	print ("Pferd ", PferdeName, " hat keinen Anspruch auf Kraftfutter")	
+function OeffneNachlaufsperre(AnlagenNummer) 
